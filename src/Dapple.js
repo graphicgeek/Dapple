b40117ac955366c50e5226d305a77ef066593c6b
Edward Ellsworth <eellsworth@mediasolutionscorp.com>
2014-10-30 11:28:49 -0700

    day.isToday and day.isInRange methods

--- src/Dapple.js
+++ src/Dapple.js
@@ -208,6 +208,19 @@ var Dapple = (function(){
             },
             beginning: function() {
                 that.date = Day.beginning(that.date);
+            },
+            isToday: function(){
+                var today = new Date();
+                if(that.date.toDateString() === today.toDateString()){ return true;}
+                return false;               
+            },
+            isInRange: function(date1, date2){
+                if(!(date1 instanceof Date)){ date1 = new Date(date1); }
+                if(!(date2 instanceof Date)){ date2 = new Date(date2);}
+
+                if(that.date >= date1 && that.date <= date2){ return true; }
+                
+                return false;
             }
         };
 
